50b0506c017b288247719d3153f40d7ec773e1ee
hongjunpyo11 <jpyo98@naver.com>
2022-07-21 13:05:42 +0900

    구구단서블릿

--- src/main/java/com/ll/exam/HelloServlet.java
+++ src/main/java/com/ll/exam/HelloServlet.java
@@ -12,14 +12,10 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 들어오는 파라미터를 UTF-8로 해석
-        req.setCharacterEncoding("UTF-8");
-        // 서블릿이 HTML 파일을 만들 때 UTF-8 로 쓰기
-        resp.setContentType("UTF-8");
+        req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8 로 해석하겠다.
+        resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-로 하겠다.
+        resp.setContentType("text/html; charset=utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8 이다 라고 알리는 의미
 
-        // HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
-        resp.setContentType("text/html; charset=utf-8");
-
-        resp.getWriter().append("안녕하세요!");
+        resp.getWriter().append("안녕하셨어요?");
     }
 }
